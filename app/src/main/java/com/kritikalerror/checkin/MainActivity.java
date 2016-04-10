1a733a5a90cfc0ddcaeff52a7ea4e07e40134c9c
mslhii <mslhii@yahoo.com>
2016-04-10 00:45:43 -0700

    Trying out the SQLite version but will probably revert to scraping the contacts list

--- app/src/main/java/com/kritikalerror/checkin/MainActivity.java
+++ app/src/main/java/com/kritikalerror/checkin/MainActivity.java
@@ -1,13 +1,66 @@
 package com.kritikalerror.checkin;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.database.Cursor;
+import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
 
-public class MainActivity extends AppCompatActivity {
+
+public class MainActivity extends ActionBarActivity {
+    public final static String KEY_EXTRA_CONTACT_ID = "KEY_EXTRA_CONTACT_ID";
+
+    private ListView listView;
+    DBHelper dbHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = (Button) findViewById(R.id.addNew);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, CreateOrEditActivity.class);
+                intent.putExtra(KEY_EXTRA_CONTACT_ID, 0);
+                startActivity(intent);
+            }
+        });
+
+        dbHelper = new DBHelper(this);
+
+        final Cursor cursor = dbHelper.getAllPersons();
+        String [] columns = new String[] {
+                DBHelper.PERSON_COLUMN_ID,
+                DBHelper.PERSON_COLUMN_NAME
+        };
+        int [] widgets = new int[] {
+                R.id.personID,
+                R.id.personName
+        };
+
+        SimpleCursorAdapter cursorAdapter = new SimpleCursorAdapter(this, R.layout.person_info,
+                cursor, columns, widgets, 0);
+        listView = (ListView)findViewById(R.id.listView1);
+        listView.setAdapter(cursorAdapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> listView, View view,
+                                    int position, long id) {
+                Cursor itemCursor = (Cursor) MainActivity.this.listView.getItemAtPosition(position);
+                int personID = itemCursor.getInt(itemCursor.getColumnIndex(DBHelper.PERSON_COLUMN_ID));
+                Intent intent = new Intent(getApplicationContext(), CreateOrEditActivity.class);
+                intent.putExtra(KEY_EXTRA_CONTACT_ID, personID);
+                startActivity(intent);
+            }
+        });
+
     }
+
 }
