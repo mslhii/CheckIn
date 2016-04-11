9d0463e725866e48acc2dd4b0d8250122faf697b
mslhii <mslhii@yahoo.com>
2016-04-11 00:16:42 -0700

    Beginning to revert code to scraping contacts list

--- app/src/main/java/com/kritikalerror/checkin/MainActivity.java
+++ app/src/main/java/com/kritikalerror/checkin/MainActivity.java
@@ -1,14 +1,21 @@
 package com.kritikalerror.checkin;
 
+import android.content.ContentResolver;
 import android.content.Intent;
 import android.database.Cursor;
+import android.net.Uri;
+import android.provider.ContactsContract;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -16,6 +23,7 @@ public class MainActivity extends ActionBarActivity {
 
     private ListView listView;
     DBHelper dbHelper;
+    private ArrayList<String[]> dbList = new ArrayList<String[]>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,9 +34,7 @@ public class MainActivity extends ActionBarActivity {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, CreateOrEditActivity.class);
-                intent.putExtra(KEY_EXTRA_CONTACT_ID, 0);
-                startActivity(intent);
+                Toast.makeText(getApplicationContext(), "Checking in!", Toast.LENGTH_SHORT).show();
             }
         });
 
@@ -40,14 +46,14 @@ public class MainActivity extends ActionBarActivity {
                 DBHelper.PERSON_COLUMN_NAME
         };
         int [] widgets = new int[] {
-                R.id.personID,
-                R.id.personName
+                R.id.personID
         };
 
-        SimpleCursorAdapter cursorAdapter = new SimpleCursorAdapter(this, R.layout.person_info,
-                cursor, columns, widgets, 0);
+//        SimpleCursorAdapter cursorAdapter = new SimpleCursorAdapter(this, R.layout.person_info,
+//                cursor, columns, widgets, 0);
+        final ArrayAdapter listAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, this.dbList);
         listView = (ListView)findViewById(R.id.listView1);
-        listView.setAdapter(cursorAdapter);
+        listView.setAdapter(listAdapter);
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
 
             @Override
@@ -61,6 +67,49 @@ public class MainActivity extends ActionBarActivity {
             }
         });
 
+    }
+
+    public void fetchContacts() {
+        String phoneName = null;
+        String phoneNumber = null;
+
+        Uri CONTENT_URI = ContactsContract.Contacts.CONTENT_URI;
+        String _ID = ContactsContract.Contacts._ID;
+        String DISPLAY_NAME = ContactsContract.Contacts.DISPLAY_NAME;
+        String HAS_PHONE_NUMBER = ContactsContract.Contacts.HAS_PHONE_NUMBER;
+
+        Uri PhoneCONTENT_URI = ContactsContract.CommonDataKinds.Phone.CONTENT_URI;
+        String Phone_CONTACT_ID = ContactsContract.CommonDataKinds.Phone.CONTACT_ID;
+        String NUMBER = ContactsContract.CommonDataKinds.Phone.NUMBER;
+
+        ContentResolver contentResolver = getContentResolver();
+
+        Cursor cursor = contentResolver.query(CONTENT_URI, null, null, null, null);
+
+        // Loop for every contact in the phone
+        if (cursor.getCount() > 0) {
+
+            while (cursor.moveToNext()) {
+                String contact_id = cursor.getString(cursor.getColumnIndex( _ID ));
+                String name = cursor.getString(cursor.getColumnIndex( DISPLAY_NAME ));
+
+                int hasPhoneNumber = Integer.parseInt(cursor.getString(cursor.getColumnIndex( HAS_PHONE_NUMBER )));
+
+                if (hasPhoneNumber > 0) {
+                    //output.append("\n First Name:" + name);
+
+                    // Query and loop for every phone number of the contact
+                    Cursor phoneCursor = contentResolver.query(PhoneCONTENT_URI, null, Phone_CONTACT_ID + " = ?", new String[] { contact_id }, null);
+                    while (phoneCursor.moveToNext()) {
+                        phoneNumber = phoneCursor.getString(phoneCursor.getColumnIndex(NUMBER));
+                        //output.append("\n Phone number:" + phoneNumber);
+                    }
+                    phoneCursor.close();
+                }
+                //output.append("\n");
+            }
+            //outputText.setText(output);
+        }
     }
 
 }
